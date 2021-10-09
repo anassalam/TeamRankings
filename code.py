5a251bc9d39f501a408b319e2f65086b24bb8fe0
@x$ <57186835+axsd5t@users.noreply.github.com>
2021-10-09 23:22:46 +0500

    code update

--- code.py
+++ code.py
@@ -9,6 +9,11 @@ cricket = []
 
 with open('text.txt', 'r') as file:
   print(cricket)
+  
+  for line in file:
+      x = line.strip()
+      url = 'http://www.espncricinfo.com/' + x.split(',')[0] + '/content/player/country.html?country=' + x.split(',')[1]
+      print(url)
 
 with open('text.json', 'w', encoding='utf-8') as file:
     json.dump(team, file, ensure_ascii=False, indent=2)
